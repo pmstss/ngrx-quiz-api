4807cc2824c604ea6bbc9a851eddfad7a2e6ba00
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-17 22:39:34 +0300

    mongodb syntax bugfix

--- src/db/auth-repo.ts
+++ src/db/auth-repo.ts
@@ -11,7 +11,7 @@ export class AuthRepo {
                 email
             })
             .addFields({
-                id: '$._id'
+                id: '$_id'
             })
             .project({
                 _id: 0,
