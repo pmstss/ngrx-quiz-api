70edd12bcb92e45ba2b3b3fd3b61b1d14b2b2b04
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-30 21:45:21 +0300

    minor renaming

--- src/entities/auth/auth-router.ts
+++ src/entities/auth/auth-router.ts
@@ -3,11 +3,13 @@ import { AuthController } from './auth-controller';
 import { AuthRepo } from './auth-repo';
 import { TokenRepo } from '../token/token-repo';
 
-const authController = new AuthController(new AuthRepo(), new TokenRepo());
+const controller = new AuthController(new AuthRepo(), new TokenRepo());
 
-export const authRouter = Router();
-authRouter.post('/login', authController.login.bind(authController));
-authRouter.post('/logout', authController.logout.bind(authController));
-authRouter.post('/register', authController.register.bind(authController));
-authRouter.post('/refresh-token', authController.refreshToken.bind(authController));
-authRouter.post('/reset', authController.resetPass.bind(authController));
+const router = Router();
+router.post('/login', controller.login.bind(controller));
+router.post('/logout', controller.logout.bind(controller));
+router.post('/register', controller.register.bind(controller));
+router.post('/refresh-token', controller.refreshToken.bind(controller));
+router.post('/reset', controller.resetPass.bind(controller));
+
+export const authRouter = router;
