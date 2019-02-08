1bb9aad2c65cd1f49e7a7e0d909ef2e8ed1bb7a6
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-08 19:21:14 +0300

    allow /quiz/:shortName access without token

--- src/entities/quiz/quiz-router.ts
+++ src/entities/quiz/quiz-router.ts
@@ -7,7 +7,7 @@ const controller = new QuizController(new QuizRepo());
 
 const router = Router();
 router.get('/', controller.getQuizList.bind(controller));
-router.get('/:shortName', tokenGuard, controller.getQuiz.bind(controller));
+router.get('/:shortName', controller.getQuiz.bind(controller));
 router.post('/reset/:quizId', tokenGuard, controller.resetQuizState.bind(controller));
 
 export const quizRouter = router;
